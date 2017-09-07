161ee2b1106f99450276d8b93c0840461b57fabb
isho <idoshoshani@gmail.com>
2017-09-07 13:59:21 -0700

    integrated geocoding

--- LocalEnv.js
+++
@@ -1,9 +0,0 @@
-(function (window){
-
-  window.__env = window.__env || {};
-
-  window.__env.API_url = 'http://localhost:8080/blog/all';
-  window.__env.baseUserName = "User";
-  window.__env.basePassWord = "OneOfTheBasicPasses";
-  window.__env.githubEventURL = 'https://api.github.com/users/ishoshani/events/public'
-}(this));
